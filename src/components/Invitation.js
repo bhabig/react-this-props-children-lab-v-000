c1c76d5c91ff011e6c493de9419721f173962e30
Bob Habig <rfhab2@gmail.com>
2018-02-08 02:34:52 -0500

    Done.

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,21 +1,12 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 const Invitation = (props) => {
   return(
     <div>
       <h1>You've been invited!</h1>
-      <div>{props.children}</div>
+      <h3>{props.children}</h3>
     </div>
   )
 }
-
-// Invitation.defaultProps = {
-//
-// };
-//
-// Invitation.propTypes = {
-//
-// };
 
 export default Invitation;
--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-const ThemedDecoration = (props) => {
-  let themedChildren = React.Children.map(props.children, child => {
-    return React.cloneElement(child, {
+const ThemedDecorations = (props) => {
+  const themedDecs = React.Children.map(props.children, child =>
+    React.cloneElement(child, {
       className: props.theme,
-    });
-  });
-
+    })
+  )
   return(
     <div>
-      {themedChildren}
+      {themedDecs}
     </div>
   )
 }
 
-// ThemedDecoration.defaultProps = {
-//
-// };
-//
-// ThemedDecoration.propTypes = {
-//
-// };
-
-export default ThemedDecoration;
+export default ThemedDecorations;
